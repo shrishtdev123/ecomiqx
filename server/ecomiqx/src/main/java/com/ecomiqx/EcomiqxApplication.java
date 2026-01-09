a2163380859fffab5b57adaf777de3bba34657e5
tecresearch <bnlv1212@gmail.com>
2026-01-09 08:48:34 -0500

    Server ecomiqx spring-boot initialized with spring-web, spring-dev-tools

---
+++ server/ecomiqx/src/main/java/com/ecomiqx/EcomiqxApplication.java
@@ -0,0 +1,13 @@
+package com.ecomiqx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomiqxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomiqxApplication.class, args);
+	}
+
+}
---
+++ server/ecomiqx/src/test/java/com/ecomiqx/EcomiqxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomiqx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomiqxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
