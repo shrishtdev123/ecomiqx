c4f84242cc85cd8fa0c4f5865df203d0f6bc6349
shrishtdev <shrishtdev@gmail.com>
2026-01-10 15:56:04 +0530

    status code 200 is added successfully in the TestController

--- server/ecomiqx/src/main/java/com/ecomiqx/required/TestController.java
+++ server/ecomiqx/src/main/java/com/ecomiqx/required/TestController.java
@@ -1,5 +1,7 @@
 package com.ecomiqx.required;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
         @GetMapping
-       public String getMessage(){
-
-            return "first api is working fine";
+       public ResponseEntity<String> getMessage()
+        {
+            return ResponseEntity.status(HttpStatus.OK).body("first api is working fine");
        }
 }
